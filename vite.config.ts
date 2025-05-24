46ded12960e6858c0c5dd71455b9108368245eb5
farkuy <darkuy@mail.ru>
2025-05-24 22:52:34 +0300

    added absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    base: "./",
     plugins: [react()],
     define: {
       "process.env": env,
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
   };
 });
